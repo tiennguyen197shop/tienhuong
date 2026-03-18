0692820824e456baa0b59b30867c9a8fdc9e6932
tiennguyen197shop <tiennguyen197shop@gmail.com>
2026-03-18 17:22:57 +0700

    update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -796,7 +796,7 @@ function startCountdown(){
 startCountdown();
 
 /* Video */
-const videoLink = "https://www.youtube.com/watch?v=uQzGdzzDtLA";
+const videoLink = "https://youtu.be/KcwuJFNWy2M";
 // hoặc
 // const videoLink = "videos/wedding.mp4";
 const videoBox = document.getElementById("videoItem");
