510e16fe052e7807d9bdb80bb8c9bbf941499bba
tiennguyen197shop <tiennguyen197shop@gmail.com>
2026-03-28 09:20:58 +0700

    update datahome.js

--- assets/js/datahome.js
+++ assets/js/datahome.js
@@ -10,6 +10,12 @@ let weddingData = null;
 LOAD JSON
 ========================= */
 
+function autoOpenTabFromURL(){
+  console.log("Hàm autoOpenTabFromURL đã chạy");
+  const host = window.location.hostname;
+  alert(host);
+}
+
 fetch("assets/data/lichth.json")
 .then(res => res.json())
 .then(data => {
